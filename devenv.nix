af5f2af191e4692fc7bf2611a2983a38fd8186c0
Dax Harris <git@dax.gay>
2026-01-31 10:32:01 -0500

    env setup

---
+++ devenv.nix
@@ -0,0 +1,45 @@
+{
+    pkgs,
+    inputs,
+    ...
+}:
+
+{
+    packages = with pkgs; [
+        git
+        cargo-autoinherit
+        webkitgtk_4_1
+        nixgl.auto.nixGLDefault
+        zellij
+    ];
+    overlays = [
+        inputs.nixgl.overlays.default
+    ];
+    languages = {
+        rust.enable = true;
+        javascript = {
+            enable = true;
+            pnpm.enable = true;
+        };
+        typescript.enable = true;
+    };
+    scripts.dev.exec = ''
+        zellij -l dev.kdl
+    '';
+    scripts.install.exec = ''
+        cd carcosa
+        pnpm install
+    '';
+    files."dev.kdl".text = ''
+        layout {
+            pane split_direction="vertical"  {
+                pane {
+                    command "nixGL"
+                    args "pnpm" "tauri" "dev"
+                    cwd "carcosa"
+                }
+                pane focus=true cwd="carcosa"
+            }
+        }
+    '';
+}
